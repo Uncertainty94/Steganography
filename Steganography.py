9088dc1f903d640a367747b1ed9d92f237fc727f
Max <mishanin_max@mail.ru>
2016-04-16 14:10:51 +0300

    another var

--- Steganography.py
+++ Steganography.py
@@ -1,13 +1,22 @@
+# -*- coding: utf-8 -*-
 from PIL import Image, ImageDraw
+res_pixels = []
 
 
+# +
 def new_blue(pixel, bit):
-    lam = 0.1
-    yxy = 0.3*pixel[0]+0.59*pixel[1]+0.11*pixel[2]
+    lam = 0.1  # then bigger lambda, then data in image are more visible and protected
+    yxy = 0.298*pixel[0]+0.586*pixel[1]+0.114*pixel[2]
+    # if blue component is 0
+    if yxy == 0:
+        yxy = 5/lam
+
     if bit == 1:
         result = pixel[2] + lam * yxy
     else:
         result = pixel[2] - lam * yxy
+
+    # if component is over
     if result > 255:
         result = 255
     if result < 0:
@@ -19,80 +28,92 @@ def new_blue(pixel, bit):
 def get_bin_code_of_char(symb):
     code = bin(ord(symb))
     code = code[2:]
-    return "0" * (8 - len(code)) + code
+    return '0' * (8 - len(code)) + code
 
 
-# Returns int equivalent of bin code of a string.
+# Returns bin equivalent of a string.
 def get_bin_code_of_string(string):
     bin_form = ''
     for char in string:
         bin_form += get_bin_code_of_char(char)
-    return int('0b'+bin_form, 2)
+    return bin_form  # int('0b' + bin_form, 2)
 
 
 def encoding(string_in):
-    string = string_in
+    string = get_bin_code_of_string(string_in)
+    str_length = len(string_in)
+    r = 5  # Количество встраиваний каждого бита сообщения
+
     image = Image.open("test.jpg")
     draw = ImageDraw.Draw(image)
     width = image.size[0]
     height = image.size[1]
     pix = image.load()
 
+    coord = []
     for i in range(3, width-3, 4):
         for j in range(3, height-3, 4):
-            a = pix[i, j][0]
-            b = pix[i, j][1]
+            coord.append([i, j])
+
+    for j in range(str_length):
+        for iteration in range(r):
+            red = pix[coord[j+iteration][0], coord[j+iteration][1]][0]
+            green = pix[coord[j+iteration][0], coord[j+iteration][1]][1]
             # print pix[i, j]
-            if len(string) != 0:
-                c = int(new_blue(pix[i, j], int(string[0])))
-                string = string[1:]
-                draw.point((i, j), (a, b, c))
-            else:
-                break
+            blue = int(new_blue(pix[coord[j+iteration][0], coord[j+iteration][1]], int(string[j])))
+            draw.point((coord[j+iteration][0], coord[j+iteration][1]), (red, green, blue))
+            res_pixels.append([coord[j+iteration], blue])
     image.save("ans.jpg", "JPEG")
     del draw
 
 
-def decoding(length):
-    count = length
+def count_blue_value(pix, i, j):
+    return (pix[i-1, j][2] + pix[i-2, j][2] + pix[i-3, j][2] + pix[i+1, j][2] +
+            pix[i+2, j][2] + pix[i+3, j][2] + pix[i, j-1][2] + pix[i, j-2][2] +
+            pix[i, j-3][2] + pix[i, j+1][2] + pix[i, j+2][2] + pix[i, j+3][2]) / 12
+
+
+def decoding(length_message):
+    count = length_message
+    r = 5
     image = Image.open("ans.jpg")
-    # draw = ImageDraw.Draw(image)
-    width = image.size[0]
-    height = image.size[1]
     pix = image.load()
 
-    old_image = Image.open("test.jpg")
-    old_pix = old_image.load()
-
     result = ''
-    for i in range(3, width-3, 4):
-        for j in range(3, height-3, 4):
-            # print 'new', pix[i, j]
-            if count > 0:
-                current_pix = pix[i, j]
-                old_pixels = old_pix[i, j]
-                val = (pix[i-1, j][2] + pix[i-2, j][2] + pix[i-3, j][2] + pix[i+1, j][2] +
-                       pix[i+2, j][2] + pix[i+3, j][2] + pix[i, j-1][2] + pix[i, j-2][2] +
-                       pix[i, j-3][2] + pix[i, j+1][2] + pix[i, j+2][2] + pix[i, j+3][2]) / 12
-                if pix[i, j][2] >= val:
-                    result += '1'
+    for i in range(len(res_pixels[0])):
+        if count > 0:
+            prog_values = []
+            for iteration in range(r):
+                x = res_pixels[0][i][0]
+                y = res_pixels[0][i][1]
+                current_pix = pix[x, y]
+                val = count_blue_value(current_pix, x, y)
+                diff = current_pix[2] - val
+
+                if diff == 0 and current_pix[2] == 255:
+                    diff = 0.5
+                if diff == 0 and current_pix[2] == 0:
+                    diff = -0.5
+
+                if diff > 0:
+                    prog_values.append(1)
                 else:
-                    result += '0'
-                count -= 1
-            else:
-                break
-    # del draw
-    print result
+                    prog_values.append(0)
+            result += str(int(round(sum(prog_values) / float(r))))
+            count -= 1
+        else:
+            break
+    print(result)
     return result
 
 
-in_string = '101011111'
-print in_string
-encoding(in_string)
-out = decoding(len(in_string))
+input_str = 'ashfb,d,fk'
+print("Input: %s" % input_str)
+encoding(input_str)
+out = decoding(len(input_str))
 
 count = 0
 for i in range(len(out)):
-    if in_string[i] != out[i]:
+    if input_str[i] != out[i]:
         count += 1
-print count/(1.0 * len(out)) * 100
+print (count / (1.0 * len(out)) * 100)
